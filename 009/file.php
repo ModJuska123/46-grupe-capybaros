a31933771c6520e9d81e893b2ab6f1ec5f2b95d6
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-17 17:39:30 +0200

    Update

--- 009/file.php
+++ 009/file.php
@@ -1,4 +1,11 @@
 <?php
 
 
-file_put_contents('file.txt', 'Are you still here');
+// file_put_contents('file.txt', 'Are you still here?');
+
+$fileContent = file_get_contents('https://www.delfi.lt/');
+
+$fileContent = str_replace('Naujienos', 'Bebrai Jėga', $fileContent);
+$fileContent = str_replace('charset=Windows-1257', 'charset=UTF-8', $fileContent);
+
+echo $fileContent;
