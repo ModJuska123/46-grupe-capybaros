53a7f330ddaf40f8fbb3e8b9c72d80f685a6eb53
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-17 13:57:13 +0200

    Update

--- Homework/ciklai.php
+++ Homework/ciklai.php
@@ -80,13 +80,13 @@ echo'<br><br><br>';
 </body>
 </html>
 
-// if (i % 77 == 0) return i else return null;
+<!-- // if (i % 77 == 0) return i else return null; -->
 
 
 echo '<br>';
-// 4. uzdavinys
+<!-- // 4. uzdavinys -->
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -94,6 +94,7 @@ echo '<br>';
     <style>
         body {
             display: flex;
+            
             justify-content: center;
             align-items: center;
             height: 100vh;
@@ -129,3 +130,198 @@ echo '<br>';
     ?>
 </body>
 </html>
+
+echo '<br><br><br>'; -->
+<!-- 5. uzduotis -->
+
+<!-- 6. uzduotis -->
+<?php
+
+function coinToss()
+{
+    return rand(0, 1); // 0 represents heads (herbas), 1 represents tails (skaičius)
+}
+
+// Scenario 1: Stop when heads (herbas) appears
+echo "Scenario 1: Stop when heads appears\n";
+do {
+    $result = coinToss();
+    echo $result === 0 ? "H\n" : "S\n";
+} while ($result !== 0);
+echo "\n";
+
+// Scenario 2: Stop after three consecutive heads (herbas)
+echo "Scenario 2: Stop after three consecutive heads\n";
+$headsCount = 0;
+do {
+    $result = coinToss();
+    echo $result === 0 ? "H\n" : "S\n";
+    $headsCount = ($result === 0) ? $headsCount + 1 : 0;
+} while ($headsCount < 3);
+echo "\n";
+
+// Scenario 3: Stop after three consecutive heads (herbas) in a row
+echo "Scenario 3: Stop after three consecutive heads in a row\n";
+$consecutiveHeadsCount = 0;
+do {
+    $result = coinToss();
+    echo $result === 0 ? "H\n" : "S\n";
+    $consecutiveHeadsCount = ($result === 0) ? $consecutiveHeadsCount + 1 : 0;
+} while ($consecutiveHeadsCount < 3);
+echo "\n";
+?>
+
+echo '<br><br><br>';
+<!-- 7. uzduotis? -->
+
+<?php
+
+function generatePoints($min, $max) {
+    return rand($min, $max);
+}
+
+function playBingo() {
+    $kazysPoints = 0;
+    $petrasPoints = 0;
+
+    while ($kazysPoints < 222 && $petrasPoints < 222) {
+        // Kazys and Petras collect points
+        $kazysPoints += generatePoints(5, 25);
+        $petrasPoints += generatePoints(10, 20);
+
+        // Display the current status
+        echo "Kazys: $kazysPoints points, Petras: $petrasPoints points<br>";
+
+        // Determine the winner (if any)
+        if ($kazysPoints >= 222) {
+            echo " Partiją laimėjo: Kazys";
+        } elseif ($petrasPoints >= 222) {
+            echo " Partiją laimėjo: Petras";
+        }
+    }
+}
+
+// Start the game
+playBingo();
+
+?>
+
+
+echo '<br><br><br>';
+<!-- 8. uzduotis -->
+<!-- 9. uzduotis -->
+<!-- 10. uzduotis -->
+
+<?php
+
+// Function to simulate hammering a nail with small strikes
+function smallStrikes($nailLength)
+{
+    $totalStrikes = 0;
+    
+    while ($nailLength > 0) {
+        $strike = rand(5, 20); // Small strikes: 5-20 mm
+        $nailLength -= $strike;
+        $totalStrikes++;
+    }
+
+    return $totalStrikes;
+}
+
+// Function to simulate hammering a nail with large strikes and a chance to miss
+function largeStrikes($nailLength)
+{
+    $totalStrikes = 0;
+
+    while ($nailLength > 0) {
+        $strike = rand(20, 30); // Large strikes: 20-30 mm
+        $hitProbability = rand(0, 1); // 0 or 1 to simulate 50% chance
+
+        if ($hitProbability) {
+            $nailLength -= $strike;
+            $totalStrikes++;
+        }
+    }
+
+    return $totalStrikes;
+}
+
+// Simulate hammering 5 small nails
+$totalSmallStrikes = 0;
+
+for ($i = 0; $i < 5; $i++) {
+    $nailLength = 85; // 8.5 cm = 85 mm
+    $totalSmallStrikes += smallStrikes($nailLength);
+}
+
+echo "Total small strikes needed for 5 nails: $totalSmallStrikes\n";
+
+// Simulate hammering 5 large nails
+$totalLargeStrikes = 0;
+
+for ($i = 0; $i < 5; $i++) {
+    $nailLength = 85; // 8.5 cm = 85 mm
+    $totalLargeStrikes += largeStrikes($nailLength);
+}
+
+echo "Total large strikes needed for 5 nails: $totalLargeStrikes\n";
+
+?>
+
+echo '<br><br><br>';
+<!-- 11. uzduotis -->
+
+<?php
+
+// Function to generate a string of unique random numbers
+function generateRandomString($length, $min, $max)
+{
+    $numbers = range($min, $max);
+    shuffle($numbers);
+
+    $uniqueNumbers = array_slice($numbers, 0, $length);
+
+    return implode(' ', $uniqueNumbers);
+}
+
+// Function to filter and order prime numbers from a string
+function filterAndOrderPrimes($inputString)
+{
+    $numbers = explode(' ', $inputString);
+    $primeNumbers = array_filter($numbers, function ($number) {
+        return isPrime($number);
+    });
+
+    sort($primeNumbers);
+
+    return implode(' ', $primeNumbers);
+}
+
+// Function to check if a number is prime
+function isPrime($number)
+{
+    if ($number < 2) {
+        return false;
+    }
+
+    for ($i = 2; $i <= sqrt($number); $i++) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Generate a string of 50 unique random numbers from 1 to 200
+$randomString = generateRandomString(50, 1, 200);
+
+// Filter and order prime numbers from the generated string
+$primeString = filterAndOrderPrimes($randomString);
+
+// Display the results
+echo "Generated Random String: " . $randomString . "\n";
+echo "Filtered and Ordered Prime String: " . $primeString . "\n";
+
+?>
+
