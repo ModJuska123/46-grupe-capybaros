6c98c9674d16146327de31985f01deb79b91e219
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-18 23:34:33 +0200

    Update

--- Homework/bankas.php
+++ Homework/bankas.php
@@ -1,135 +1,97 @@
 <?php
 
-// Functions for working with JSON data
-function readJsonData($filename)
-{
-    if (file_exists($filename)) {
-        $json_data = file_get_contents($filename);
-        return json_decode($json_data, true);
-    } else {
-        return [];
-    }
+// Sample JSON file for storing accounts
+$accountsFile = 'accounts.json';
+
+// Function to load accounts from JSON file
+function loadAccounts() {
+    global $accountsFile;
+    return json_decode(file_get_contents($accountsFile), true);
 }
 
-function writeJsonData($filename, $data)
-{
-    $json_data = json_encode($data, JSON_PRETTY_PRINT);
-    file_put_contents($filename, $json_data);
+// Function to save accounts to JSON file
+function saveAccounts($accounts) {
+    global $accountsFile;
+    file_put_contents($accountsFile, json_encode($accounts, JSON_PRETTY_PRINT));
 }
 
-// Function to generate a random IBAN number
-function generateIBAN()
-{
-    return 'LT' . rand(10, 99) . '0000' . rand(1000, 9999) . rand(1000, 9999) . rand(1000, 9999);
+// Function to generate a random IBAN
+function generateIBAN() {
+    return 'LT12' . str_pad(mt_rand(1000, 9999), 8, '0', STR_PAD_LEFT) . mt_rand(100000000, 999999999);
 }
 
 // Function to validate personal code
-function validatePersonalCode($personalCode)
-{
-    // Add your validation logic here
-    return true;
+function validatePersonalCode($personalCode) {
+    // Add validation logic here
+    // Example: Check if it's a valid format
+    return preg_match('/^\d{11}$/', $personalCode);
 }
 
-// Load existing bank data
-$bankData = readJsonData('bank_data.json');
-
-// Check if the user is logged in (for simplicity, no real authentication)
-$isLoggedIn = isset($_SESSION['user']);
-
-// Handle different pages
-if (isset($_GET['page'])) {
-    $page = $_GET['page'];
-
-    switch ($page) {
-        case 'account_list':
-            // Display a list of accounts
-            // Sort accounts by owner's last name
-            ksort($bankData);
-            // Add HTML code to display the account list
-            break;
-
-        case 'add_funds':
-            // Display the "add funds" page
-            // Add HTML code to display the form for adding funds
-            break;
-
-        case 'deduct_funds':
-            // Display the "deduct funds" page
-            // Add HTML code to display the form for deducting funds
-            break;
-
-        case 'create_account':
-            // Display the "create account" page
-            // Add HTML code to display the form for creating a new account
-            break;
-
-        default:
-            // Handle other cases or display an error page
-            break;
-    }
-} elseif (isset($_POST['action'])) {
-    // Handle form submissions
-    $action = $_POST['action'];
-
-    switch ($action) {
-        case 'create_account':
-            // Validate input data
-            $name = $_POST['name'];
-            $surname = $_POST['surname'];
-            $personalCode = $_POST['personal_code'];
-
-            if (strlen($name) > 3 && strlen($surname) > 3 && validatePersonalCode($personalCode)) {
-                // Generate a unique IBAN and create a new account
-                $iban = generateIBAN();
-                $bankData[$iban] = [
-                    'owner' => "$name $surname",
-                    'balance' => 0,
-                    'personal_code' => $personalCode,
-                ];
-                writeJsonData('bank_data.json', $bankData);
-                echo "Account created successfully!";
-            } else {
-                echo "Invalid input data!";
-            }
-            break;
-
-        case 'add_funds':
-            // Validate input data
-            $iban = $_POST['iban'];
-            $amount = $_POST['amount'];
-
-            if (isset($bankData[$iban]) && is_numeric($amount) && $amount > 0) {
-                // Update account balance
-                $bankData[$iban]['balance'] += $amount;
-                writeJsonData('bank_data.json', $bankData);
-                echo "Funds added successfully!";
-            } else {
-                echo "Invalid input data!";
-            }
-            break;
-
-        case 'deduct_funds':
-            // Validate input data
-            $iban = $_POST['iban'];
-            $amount = $_POST['amount'];
-
-            if (isset($bankData[$iban]) && is_numeric($amount) && $amount > 0 && $bankData[$iban]['balance'] >= $amount) {
-                // Update account balance
-                $bankData[$iban]['balance'] -= $amount;
-                writeJsonData('bank_data.json', $bankData);
-                echo "Funds deducted successfully!";
-            } else {
-                echo "Invalid input data!";
-            }
-            break;
-
-        default:
-            // Handle other cases or display an error page
-            break;
-    }
-} else {
-    // Display the default page or redirect to the login page
-    // Add HTML code for the default page
+// Function to validate name and surname
+function validateName($name) {
+    // Add validation logic here
+    // Example: Check if it's at least 3 characters long
+    return strlen($name) >= 3;
 }
+
+// Function to create a new account
+function createAccount($name, $surname, $personalCode) {
+    $accounts = loadAccounts();
+
+    // Validate input data
+    if (!validateName($name) || !validateName($surname) || !validatePersonalCode($personalCode)) {
+        return 'Invalid input data';
+    }
+
+    // Check if the personal code is unique
+    foreach ($accounts as $account) {
+        if ($account['personal_code'] == $personalCode) {
+            return 'Personal code must be unique';
+        }
+    }
+
+    // Generate a new IBAN
+    $iban = generateIBAN();
+
+    // Create a new account
+    $newAccount = [
+        'name' => $name,
+        'surname' => $surname,
+        'personal_code' => $personalCode,
+        'iban' => $iban,
+        'balance' => 0,
+    ];
+
+    // Add the new account to the accounts list
+    $accounts[] = $newAccount;
+
+    // Save the updated accounts list
+    saveAccounts($accounts);
+
+    return 'Account created successfully';
+}
+
+// Function to display the list of accounts
+function displayAccounts() {
+    $accounts = loadAccounts();
+
+    // Sort accounts by surname
+    usort($accounts, function ($a, $b) {
+        return strcmp($a['surname'], $b['surname']);
+    });
+
+    // Display the list
+    echo '<ul>';
+    foreach ($accounts as $account) {
+        echo '<li>' . $account['surname'] . ', ' . $account['name'] . ' - ' . $account['iban'] . '</li>';
+    }
+    echo '</ul>';
+}
+
+// Example usage:
+createAccount('John', 'Doe', '12345678901');
+displayAccounts();
+createAccount('Modestas', 'Juska', '12345677896');
+displayAccounts();
 
 ?>
