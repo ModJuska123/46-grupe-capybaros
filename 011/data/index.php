c1f1b94aee92278f3f9ec9a1c7152e1aa55a5eab
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-19 12:17:00 +0200

    update

---
+++ 011/data/index.php
@@ -0,0 +1,31 @@
+<?php
+echo '<pre>';
+
+$animals = require __DIR__ . '/animals.php';
+
+print_r($animals);
+
+                // $animals[1] = 'Good ' . $animals[1];
+
+                // $animals[301] = 'Good ' . $animals[1];
+
+// $json = json_encode($animals, JSON_PRETTY_PRINT);
+
+// file_put_contents(__DIR__ . '/animals.json', $json);
+
+// $getJson = file_get_contents(__DIR__ . '/animals.json');
+
+// $data = json_decode($getJson);
+
+// foreach ($data as $animal) {
+//     echo $animal . '<br>';
+// }
+// $copy = array_map(function($item) {
+//     return $item;
+// }, $data);
+
+
+print_r($copy);
+
+
+echo '</pre>';
