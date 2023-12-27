0cc80023174ede7fa766569fea410a85adba4bf2
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-27 19:41:01 +0200

    Update

--- 013/index.php
+++ 013/index.php
@@ -4,7 +4,7 @@ session_start();
 $_SESSION['mano_sesija'] = 'skanus sesijas';
 $_SESSION['logged'] = 'yes sesijas';
 
-$_SESSION['log_time'] = time();
+// $_SESSION['log_time'] = time();
 
 setcookie('mano_saldainis', 'skanus');
 setcookie('mano_saldainis2', 'dar skanesnis');
