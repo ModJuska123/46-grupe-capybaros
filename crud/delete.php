393d02a81da4a4c51bc58856927ab7edc7908a2e
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-28 23:15:44 +0200

    update

---
+++ crud/delete.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=s, initial-scale=1.0">
+    <title>Document</title>
+<style>
+        .delete {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            width: 100%;
+        }
+        .delete-container {
+            width: 400px;
+            height: 200px;
+            border-radius: 10px;
+            background-color: crimson;
+            color: white;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="delete">
+
+    </div>
+</body>
+</html>
