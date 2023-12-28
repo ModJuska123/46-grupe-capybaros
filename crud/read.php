2a293c2a771c8090a1167884acfc65a8740d743f
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-28 11:25:23 +0200

    Update

--- crud/read.php
+++ crud/read.php
@@ -4,29 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <title>Read</title>
+    <title>Create</title>
 </head>
 <body>
 
-    <nav class="navbar navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Navbar</a>
-        </div>
-    </nav>
+    <?php require __DIR__ . '/parts/nav.php' ?>
 
     <div class="container mt-5">
         <div class="row">
-            <div class="col-2">
-            Box ID
-            </div>
-            <div class="col-2">
-            Amount
-            </div>
-            <div class="col-8">
-            Action
+            <div class="col">
+                <h2>Create</h2>
+                <div class="card">
+                    <div class="card-body">
+                        <form action="http://localhost/capybaros/crud/store.php" method="post">
+                            <div class="mb-3">
+                                <label class="form-label">Amount:</label>
+                                <input type="range" class="form-range" name="amount" min="0" max="1000">
+                            </div>
+                            <button type="submit" class="btn btn-outline-primary">Create</button>
+                        </form>                            
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    
+
 </body>
 </html>
