8145c6b7066676e8b4ed262a038bcd96446352ff
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-12-29 19:58:44 +0200

    update

--- crud/read.php
+++ crud/read.php
@@ -4,30 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <title>Create</title>
+    <title>Read</title>
 </head>
 <body>
 
     <?php require __DIR__ . '/parts/nav.php' ?>
-
     <div class="container mt-5">
         <div class="row">
-            <div class="col">
-                <h2>Create</h2>
-                <div class="card">
-                    <div class="card-body">
-                        <form action="http://localhost/capybaros/crud/store.php" method="post">
-                            <div class="mb-3">
-                                <label class="form-label">Amount:</label>
-                                <input type="range" class="form-range" name="amount" min="0" max="1000">
-                            </div>
-                            <button type="submit" class="btn btn-outline-primary">Create</button>
-                        </form>                            
-                    </div>
-                </div>
+            <div class="col-2">
+                <h2>Read</h2>
             </div>
         </div>
     </div>
 
+
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                    <b>Box ID</b>
+                    </div>
+                    <div class="col-2">
+                    <b>Amount</b>
+                    </div>
+                    <div class="col-8">
+                    <b>Action</b>
+                    </div>
+                </div>
+            </div>
+        </li>
+
+        <?php $boxes = json_decode(file_get_contents(__DIR__ . '/data/boxes.json'), true) ?>
+        <?php foreach ($boxes as $box) : ?>
+        <li class="list-group-item">
+            <div class="container">
+                <div class="row">
+                    <div class="col-2">
+                        <?= $box['boxId'] ?>
+                    </div>
+                    <div class="col-2">
+                        <?= $box['amount'] ?>
+                    </div>
+                    <div class="col-8">
+                        <a href="http://localhost/capybaros/crud/show.php?id=<?= $box['boxId'] ?>" class="btn btn-outline-success btn-sm">Show</a>
+                    </div>
+                </div>
+            </div>
+        </li>
+
+        <?php endforeach ?>
+    </ul>
+    
 </body>
 </html>
