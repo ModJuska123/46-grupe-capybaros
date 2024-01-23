e0abce793b903d079b243f15dccb452070ecaf58
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-01-23 16:43:47 +0200

    Update

---
+++ db/index.php
@@ -0,0 +1,31 @@
+<?php
+
+
+$host = '127.0.0.1';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$pdo = new PDO($dsn, $user, $pass, $options);
+
+// SELECT column1, column2, ...
+// FROM table_name;
+
+$sql = "
+    SELECT id, name, height, type
+    FROM trees
+";
+
+$stmt = $pdo->query($sql);
+
+$trees = $stmt->fetchAll();
+
+echo '<pre>';
+print_r($trees);
